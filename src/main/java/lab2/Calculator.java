6b31464a420906e4fe42fd6af2d5112f076f6571
Vsev0l0d <vsevolod628@gmail.com>
2022-03-13 19:40:49 +0300

    csv

--- src/main/java/lab2/Calculator.java
+++ src/main/java/lab2/Calculator.java
@@ -1,5 +1,8 @@
 package lab2;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public abstract class Calculator {
     protected double accuracy;
 
@@ -8,4 +11,22 @@ public abstract class Calculator {
     }
 
     public abstract double calculate(double x);
+
+    public void saveToCsv(String filePath, double x, double limit, double step){
+        try(FileWriter writer = new FileWriter(filePath, false)) {
+            writer.write(toCsv(x, limit, step));
+            writer.flush();
+        } catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public String toCsv(double x, double limit, double step){
+        StringBuilder result = new StringBuilder();
+        while (x <= limit){
+            result.append(x).append(", ").append(calculate(x)).append("\n");
+            x += step;
+        }
+        return result.toString();
+    }
 }
